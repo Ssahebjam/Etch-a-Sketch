a31e0a94dd3d9933423cdcf84203d5ec0d5c5908
soheyl <soheylrashidsahebjam@gmail.com>
2025-10-25 15:48:39 +0330

    Add feature reset button & calculate width of pixel's
    Reset board and repaint it with input of user

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const board = document.getElementById("board");
+const resetBtn = document.getElementById("resetBtn");
 
 function makeBoard(num, board) {
   for (let i = 0; i < num; i++) {
@@ -6,17 +7,34 @@ function makeBoard(num, board) {
       let square = document.createElement("div");
 
       square.classList.add("square");
+      square.style.width = `${980 / num}px`;
+      square.style.height = `${980 / num}px`;
+
       board.appendChild(square);
     }
   }
 }
 
-function paintBoard(e) {
+function paintOnBoard(e) {
   if (e.target.classList.contains("square")) {
     e.target.style.backgroundColor = "red";
   }
 }
 
+function resetBoard() {
+  board.textContent = "";
+}
+
 makeBoard(16, board);
 
-board.addEventListener("mouseover", paintBoard);
+board.addEventListener("mouseover", paintOnBoard);
+
+let gridNum;
+
+resetBtn.addEventListener("click", () => {
+  do {
+    gridNum = prompt("enter you'r number");
+  } while (gridNum > 100 || gridNum <= 0);
+  resetBoard();
+  makeBoard(gridNum, board);
+});
